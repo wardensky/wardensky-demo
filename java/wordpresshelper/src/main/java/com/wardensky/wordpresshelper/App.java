390fa1bf6362d2e9c050a59c7b8496896210f2cb
wardensky <zhaochunhui1985@gmail.com>
2018-01-06 16:14:42 +0800

    a

--- java/wordpresshelper/src/main/java/com/wardensky/wordpresshelper/App.java
+++ java/wordpresshelper/src/main/java/com/wardensky/wordpresshelper/App.java
@@ -1,8 +1,8 @@
 package com.wardensky.wordpresshelper;
 
 public class App {
-	static String title = "楠楠弹古筝";
-	static String videoUrl = "http://7.happykeke.top/video_%E5%8F%A4%E7%AD%9D%E8%A7%86%E9%A2%91-1.mp4";
+	static String title = "【视频】可可在海边玩";
+	static String videoUrl = "http://7xq6cg.com1.z0.glb.clouddn.com/video_%E5%8F%AF%E5%8F%AF%E5%9C%A8%E6%B5%B7%E8%BE%B9-compress.mp4";
 
 	public static void main(String[] args) {
 		MyJfinalConfig config = new MyJfinalConfig();
